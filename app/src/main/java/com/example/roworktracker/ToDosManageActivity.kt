beda9236006fe01d7ee932915841195ad36e68f0
samudithTharindaka <samupuff@gmail.com>
2024-09-20 23:36:42 +0530

    added the menu and todo activity, removed fragment menu

--- app/src/main/java/com/example/roworktracker/ToDosManageActivity.kt
+++ app/src/main/java/com/example/roworktracker/ToDosManageActivity.kt
@@ -1,10 +1,8 @@
 package com.example.roworktracker
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -12,32 +10,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ToDosManageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_to_dos_manage)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
 
-        val homeBtn:ImageButton = this.findViewById(R.id.imageButtonMenu)
-        val settingsBtn:ImageButton = this.findViewById(R.id.imageButton3Menu)
-        val todoBtn:ImageButton = this.findViewById(R.id.imageButton2Menu)
+        val homeBtn: ImageButton = this.findViewById(R.id.imageButtonMenu)
+        val settingsBtn: ImageButton = this.findViewById(R.id.imageButton3Menu)
+        val todoBtn: ImageButton = this.findViewById(R.id.imageButton2Menu)
 
-//        handle the menu
-        homeBtn.setOnClickListener(){
-            Toast.makeText(this,"You are already in home",Toast.LENGTH_SHORT).show()
-        }
 
         settingsBtn.setOnClickListener(){
 
             Log.d("MainActivity", "Settings button clicked")
             // Check if Intent works
             try {
-                val intent = Intent(this, ToDosManageActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 Log.d("MainActivity", "Intent started successfully")
             } catch (e: Exception) {
@@ -45,13 +41,5 @@ class MainActivity : AppCompatActivity() {
             }
             Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
         }
-
-        todoBtn.setOnClickListener(){
-            Toast.makeText(this,"todo clicked",Toast.LENGTH_SHORT).show()
-        }
-
-
     }
-
-
 }
